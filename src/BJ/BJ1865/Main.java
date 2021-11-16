e07cbefa09fdcbc051ca5ff0309ca614bbff8c0f
HK Jang <90881441+tschue-ss@users.noreply.github.com>
2021-11-16 21:41:21 +0900

    [BJ18650]_웜홀_Bellman-ford

--- src/BJ/BJ1865/Main.java
+++ src/BJ/BJ1865/Main.java
@@ -1,12 +1,10 @@
 package BJ.BJ1865;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer; 
@@ -31,15 +29,15 @@ YES
 
 public class Main {
 	static int T, N, M, W, INF = (int) 21e8;
-	static ArrayList<ArrayList<Node>> Adj;
-	static long[] Answer;
+	static ArrayList<Node>[] Adj;
+	static int[] Answer;
 	
 	
 	private static class Node {
 		int idx;
-		long cost;
+		int cost;
 		
-		Node(int idx, long cost) {
+		Node(int idx, int cost) {
 			this.idx = idx;
 			this.cost = cost;
 		}
@@ -62,45 +60,39 @@ public class Main {
 			M = stoi(st.nextToken());		// 도로 (간선)
 			W = stoi(st.nextToken());		// 웜홀 (Minus 간선)
 			
-			Answer = new long[N + 1];
+			Answer = new int[N + 1];
 			Arrays.fill(Answer, INF);
 			
-			Adj = new ArrayList<>(); 	// 인접 리스트
+			Adj = new ArrayList[N + 1]; 	// 인접 리스트
 			for (int n = 0; n <= N; n++) {
-				Adj.add(new ArrayList<Node>());	// 인접리스트 초기화
+				Adj[n] = new ArrayList<Node>(); // 인접리스트 초기화
 			}
 			
-			for (int m = 0; m <= M; m++) {
+			for (int m = 0; m < M; m++) {
 				st = new StringTokenizer(br.readLine());
 				int s = stoi(st.nextToken()); // 노드_1
 				int e = stoi(st.nextToken()); // 노드_2
 				int c = stoi(st.nextToken()); // cost
 
 				// 양방향 노드 연결
-				Adj.get(s).add(new Node(e, c));
-				Adj.get(e).add(new Node(s, c));
+				Adj[s].add(new Node(e, c));
+				Adj[e].add(new Node(s, c));
 			}
 			
-			for (int w = 0; w <= W; w++) {
+			for (int w = 0; w < W; w++) {
 				st = new StringTokenizer(br.readLine());
 				int s = stoi(st.nextToken()); // 노드_1
 				int e = stoi(st.nextToken()); // 노드_2
-				int c = stoi(st.nextToken()); // cost
+				int c = stoi(st.nextToken()) * -1; // cost
 				
 				// 단방향 노드 연결
-				Adj.get(s).add(new Node(e, c));
+				Adj[s].add(new Node(e, c));
 			}
 			
 			if (bellmanFord()) {
 				System.out.println("YES");
 			} else {
-				for (int i = 2; i <= N; i++) {
-					if (Answer[i] == INF) {
-						System.out.println("NO");
-					} else {
-						System.out.println("YES");
-		            }
-	            }
+				System.out.println("NO");
 	        }
 		}
 		br.close();
@@ -114,23 +106,28 @@ public class Main {
 			updateFlag = false;
 			
 			for (int n = 1; n <= N; n++) {
-				for (Node next : Adj.get(n)) {
+				for (Node next : Adj[n]) {
 					if (Answer[n] == INF) break;
 					
-					if (Answer[next.idx] > Answer[n] + next.cost) {
-						Answer[next.idx] = Answer[n] + next.cost; 
+					int nextIdx = next.idx;
+					int nextCost = next.cost;
+					
+					if (Answer[nextIdx] > Answer[n] + nextCost) {
+						Answer[nextIdx] = Answer[n] + nextCost; 
 						updateFlag = true;
 					}
 				}
 				
 				// Cycle이 존재하지 않음
-				if (!updateFlag) break;
+				if (!updateFlag) {
+					break;
+				}
 			}
 			
 			// 음수의 Cycle이 존재
 			if (updateFlag) {
 				for (int n = 1; n <= N; n++) {
-					for (Node next : Adj.get(n)) {
+					for (Node next : Adj[n]) {
 						if (Answer[n] == INF) break;
 						
 						if (Answer[next.idx] > Answer[n] + next.cost) {
